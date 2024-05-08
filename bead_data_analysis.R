c8859b2f05afd8aea3111551d54597c1e287f255
d.schlaeppi <110820808+d-schlaeppi@users.noreply.github.com>
2024-05-08 12:41:30 +0100

    Progress with analysis
    First look at the group level flugus experiment (boris annotations from ellie) and start of the vital bead analysis.

---
+++ bead_data_analysis.R
@@ -0,0 +1,16 @@
+rm(list = ls())
+
+### ### ### ### ### ### ### ### ### ### ### 
+### Preliminary bead data analysis  ### ###
+### ### ### ### ### ### ### ### ### ### ### 
+
+#### Read Me ####
+# First rough look at the beads data to get an idea on the food consumption and spread through colonies
+
+#### Todo ####
+
+# 1. Load the data: Colony meta data, brood bead data, and individual metadata
+# Adjust bead values of the brood based substracting control mean_status
+# Assign bead colours to virus vs control food sources
+# Check for general differences in the colours (probably to include later as a random factor)
+# Check for general differences in the values per food source 
