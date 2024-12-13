a17be75fa76fd669a43f6b78ac6b40de31d5b411
d-schlaeppi <bruetschli@icloud.com>
2024-12-13 13:49:31 +0100

    Finish the FC1 script, Update the analysis of the fluorescent bead data by creating a randomization of the control pseudo virus food source assignment.

--- config_user_and_hd.R
+++ config_user_and_hd.R
@@ -174,3 +174,43 @@ read.tag <- function(tag_list, colony){
   tag <- read.table(tag,header=T,stringsAsFactors = F)
   return(tag)
 } # used in subsequent scripts to read the tag list (generated earlier) for specific colonies
+
+
+# test normal distribution of residuals in linear models:
+test_norm <- function(mod) { # function from Nathalie # resids
+  cat(blue("Testing normality of residuals for lm or lmer \n(assumptions likely different other types of models)\n"))
+  resids <- residuals(object = mod)
+  if (length(resids) <= 300) {
+    print("Fewer than 300 data points so performing Shapiro-Wilk's test")
+    shapiro_result <- shapiro.test(resids)
+    print(shapiro_result)
+    p_value <- shapiro_result$p.value
+    if(p_value >= 0.05) {
+      cat(green("Data is normally distributed\n --> Model is fine... go ahead \U1F44D"))
+    } else {
+      cat(red("Warning: p value below 0.05\n-->  Data significantly deviate from a normal distribution. \nAdjust or change model!"))
+    }
+  } else {
+    print("More than 300 data points so using the skewness and kurtosis approach")
+    print("Skewness should be between -3 and +3 (best around zero)")
+    skew <- skewness(resids)
+    print(paste0("Skewness = ", skew))
+    print("Excess kurtosis (i.e. absolute kurtosis -3) should be less than 4; ideally around zero")
+    kurt <- kurtosis(resids)
+    print(paste0("Kurtosis = ", kurt))
+    if(skew >= -3 & skew <= 3 & kurt < 4){
+      cat(green("Model is fine... go ahead \U1F44D"))
+    } else {
+      cat(red("Warning: Adjust or change model!"))
+    }
+  }
+}
+
+overdispersion_test <- function(model) {
+  rdf <- df.residual(model)
+  rp <- residuals(model, type = "pearson")
+  Pearson_chisq <- sum(rp^2)
+  prat <- Pearson_chisq / rdf
+  pval <- pchisq(Pearson_chisq, df = rdf, lower.tail = FALSE)
+  c(chisq = Pearson_chisq, ratio = prat, rdf = rdf, p = pval)
+}
